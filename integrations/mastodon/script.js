899057b50e5221bbec9b51da8599546c54ba8b2b
Kimberly Grey <kimandthemachine@gmail.com>
2018-11-29 17:10:40 +0000

    Style Mastodon integration

--- integrations/mastodon/script.js
+++ integrations/mastodon/script.js
@@ -1,8 +1,6 @@
 /**
  * TODO: 
- *   Garbage collection after 15-20 toots.
  *   Custom emoji???
- *   Prettify.
  */
 
 class Mastodon {
@@ -12,7 +10,7 @@ class Mastodon {
 			instance: this.settings.instance,
 			api_user_token: this.settings.apiToken
 		});
-		this.$timeline = $('#timeline');
+		this.$timeline = $('.timeline');
 		// Trackin' things
 		this.lastStatusId = 0; // tracks the last status ID we received, so we don't get dupes 
 		// Start the thing
@@ -53,34 +51,50 @@ class Mastodon {
 		const tootTemplate = `
 			<li id="toot-${toot.id}">
 				<article class="toot">
-					<header class="toot__header">
-						${ (reblogger) ? `<div class="toot__rebloggedby">Boosted by ${reblogger.account.display_name}</div>` : '' }
-						<div class="toot__user">
-							<img class="toot__user-avatar" src="${toot.account.avatar}" alt="${toot.account.display_name}">
-							<strong class="toot__user-name">${toot.account.display_name}</strong>
+					${ (reblogger) ? `<div class="size-small toot__rebloggedby">🔁 Boosted by ${reblogger.account.display_name}</div>` : '' }
+					<header class="size-small toot__header">
+						<img class="toot__avatar" src="${toot.account.avatar}" alt="${toot.account.display_name}">
+						<span class="toot__user">
+							<strong class="weight-600 toot__user-name">${toot.account.display_name}</strong>
 							<span class="toot__user-handle">@${toot.account.acct}</span>
-						</div>
-						<time class="toot__date" datetime="${toot.created_at}">${toot.created_at}</time>
+						</span>
+						<time class="font-narrow toot__date" datetime="${toot.created_at}">${toot.created_at}</time>
 					</header>
 					<div class="toot__body">${toot.content}</div>
 					${ (toot.media_attachments.length > 0) ? this.parseMediaAttachments(toot.media_attachments) : '' }
 				</article>
 			</li>
 		`;
+		// add to the list
 		this.$timeline.prepend(tootTemplate);
+		// remove old ones
+		this.$timeline.children(':nth-child(n+11)').remove();
+		// re-init timeago
+		$('.toot__date').timeago();
 	}
 	parseMediaAttachments(mediaArray) {
-		let returnHtml = '';
+		let returnHtml = '<div class="toot__media">';
 		$.each(mediaArray, (i, media) => {
 			switch(media.type) {
 				case 'image':
-					returnHtml += `<img src="${media.preview_url}" alt="${media.description}">`;
+					returnHtml += `
+						<div class="toot__image">
+							<img src="${media.preview_url}" alt="${ (media.description) ? media.description : '' }" width="${media.meta.small.width}" height="${media.meta.small.height}">
+						</div>
+					`;
 				break;
 				case 'video':
-					returnHtml += `<video autoplay muted loop width="${media.meta.width}" height="${media.meta.height}"><source src="${media.remote_url}"></video>`;
+					returnHtml += `
+						<div class="toot__video" style="padding-top:${(media.meta.height / media.meta.width) * 100}%">
+							<video autoplay muted loop width="${media.meta.width}" height="${media.meta.height}">
+								<source src="${media.remote_url}" type="video/mp4">
+							</video>
+						</div>
+					`;
 				break;
 			}
 		});
+		returnHtml += '</div>';
 		return returnHtml;
 	}
 }
